a425ff852eb9bffca70cf5960c3a1f5a150ecf52
rainandray-netizen <ryan.d.adrian.27@gmail.com>
2019-10-28 14:21:03 -0400

    chore: initial commit

---
+++ js/app.js
@@ -0,0 +1,32 @@
+//https://api.nytimes.com/svc/topstories/v2/${$section}.json?api-key=TrCrwocaRjaBrbFZ6mGpchM9exLSH5Qs
+//https://api.nytimes.com/svc/topstories/v2/science.json?api-key=yourkey
+
+$(function(){
+    $("select").on("change", function(event){
+        $("header").removeClass("headerinit").addClass("minidisplay")
+        $(".articlegrid").html("")
+        $section = ($(this).val())
+        $.getJSON(`https://api.nytimes.com/svc/topstories/v2/${$section}.json?api-key=TrCrwocaRjaBrbFZ6mGpchM9exLSH5Qs`)
+        .done(function(data){
+            console.log(data)
+            $title = $(data.results)
+            let counter = 0
+            $.each($title, function(index,value){
+                if($title[index].multimedia.length !==0 && counter<12){
+                    counter++
+                    $(".articlegrid").append(
+                        `<div class="article" style="background:url('${$title[index].multimedia[4].url}'); background-size:cover;background-repeat: no-repeat;background-position: center center;">
+                            <div class="shadowbox">
+                                <a target="_blank" href="${$title[index].url}" class="title">${$title[index].abstract}</a>
+                            </div>
+                        </div>`)
+                }
+            })
+        }) 
+        .fail(function(){
+            alert("Its Knuckles. You're in a comma and I have been trying to get this mesage to you for years..From what doctors discovered in your dreams you believe that I am some sort of game character but i am actually your husband. Please baby, wake up.")
+        })
+    })
+    
+    
+})
